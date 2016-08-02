2d70348f8c319e348df25dc7869a6602b5a00221
Rahul Madhavan <rahul.maddy@gmail.com>
2016-08-02 12:06:31 +0530

    corrected errors in mergesort test

---
+++ PriorityQueue.py
@@ -0,0 +1,117 @@
+# A priority queue is nothing but a heap data structure.
+# A priority queue is all about trade-offs. See http://en.wikipedia.org/wiki/Priority_queue
+#
+# You should choose one of these two, based on how you plan to use it:
+#
+# O(log(N)) insertion time and O(1) findMin+deleteMin time, or
+# O(1) insertion time and O(log(N)) findMin+deleteMin time
+# In the latter case, you can choose to implement a priority queue with a Fibonacci heap:
+# http://en.wikipedia.org/wiki/Heap_(data_structure)#Comparison_of_theoretic_bounds_for_variants
+# (as you can see, heapq which is basically a binary tree, must necessarily have O(log(N)) for both
+# insertion and findMin+deleteMin)
+#
+# From: https://github.com/danielborowski/fibonacci-heap-python
+# They are complicated when it comes to coding them. Also they are not as efficient in practice when 
+# compared with the theoretically less efficient forms of heaps, since in their simplest version they require 
+# storage and manipulation of four pointers per node, compared to the two or three pointers per node needed 
+# for other structures
+# We therefore code the priority queue as a binary heap and added the fibonacci heap as a separate structure
+import time
+class PriorityQueue(object):
+    def __init__(self,inputArray=None):
+        if inputArray == None:
+            self.heapList = []
+            self.currentSize = 0
+        else:
+            self.heapList = inputArray
+            self.buildHeap(self.heapList)
+            self.currentSize = len(inputArray)
+
+    def percUp(self,node_index):
+        while (node_index - 1)//2 >= 0:
+            if self.heapList[node_index] < self.heapList[(node_index - 1)//2]:
+                self.__swap(node_index,(node_index - 1)//2)
+            node_index = (node_index - 1)//2
+
+    def __swap(self,firstindex,secondindex):
+         tmp = self.heapList[secondindex]
+         self.heapList[secondindex] = self.heapList[firstindex]
+         self.heapList[firstindex] = tmp
+
+    def insert(self,insertElement):
+        self.heapList.append(insertElement)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize-1)
+
+    def percDown(self,node_index):
+        while (node_index*2 + 1) < self.currentSize:
+            minchild = self.minchild(node_index)
+            if self.heapList[node_index] > self.heapList[minchild]:
+                self.__swap(node_index,minchild)
+            node_index = minchild
+
+    def minchild(self,node_index):
+        if (node_index + 1)*2 >= self.currentSize:
+            return node_index*2 + 1
+        else:
+            if self.heapList[node_index*2 + 1] < self.heapList[(node_index + 1)*2]:
+                return node_index*2 + 1
+            else:
+                return (node_index + 1)*2
+
+    def extractMin(self):
+        min = self.heapList[0]
+        self.heapList[0] = self.heapList[self.currentSize-1]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(0)
+        return min
+
+    def buildHeap(self,input_list):
+        itercounter = len(input_list) // 2
+        self.currentSize = len(input_list)
+        self.heapList = input_list[:]
+        while (itercounter >= 0):
+            self.percDown(itercounter)
+            itercounter = itercounter - 1
+
+    def show(self):
+        print(self.heapList)
+
+    def __str__(self):
+        return str(self.heapList)
+
+
+if __name__ == "__main__":
+    # test 1
+    print("test 1")
+    timestart = time.time()
+    for i in range(100000):
+        p = PriorityQueue()
+        p.insert(10)
+        p.insert(5)
+        p.insert(2)
+        p.insert(100)
+        p.insert(1)
+        p.insert(12)
+
+    print(p)
+    print("time taken = ",time.time()-timestart)
+
+    # test 2
+    print("test 2")
+    timestart = time.time()
+    for i in range(100000):
+        p = PriorityQueue()
+        p.insert((10,"hi10"))
+        p.insert((5,"hi5"))
+        p.insert((2,"yo"))
+        p.insert((100,"tendulkar"))
+        p.insert((1,"uno"))
+        p.insert((12,"dozen"))
+
+    print(p)
+    print("time taken = ",time.time()-timestart)
+
+
+
---
+++ SortedArray.py
@@ -0,0 +1,106 @@
+import time
+import quicksort
+time = time.time()
+
+class SortedArray(object):
+    def __init__(self,inputArray = None):
+        if inputArray != None:
+            self.array = []
+            self.size = 0
+        elif type(inputArray) is list:
+            self.array = inputArray
+            self.sortarray()
+            self.size = len(inputArray)
+        else: raise TypeError("Unable to initialize, please input list")
+
+    def sortarray(self):
+        self.array = quicksort(self.array)
+
+    def show(self):
+        print(self.array)
+
+    def inputlist(self,inputArray):
+        self.array = inputArray
+        self.sortarray()
+        self.size = len(inputArray)
+
+    def __str__(self):
+        return str(self.array)
+    def insertval(self,value):
+        rank = self.rank(value)
+        newarray = self.array[:rank+1] + [value] + self.array[rank + 1:]
+        self.array = newarray
+        self.size = self.size +1
+        return
+
+
+
+    def deleteindex(self,index):
+        newarray = self.array[:index] + self.array[index+1:]
+        self.array = newarray
+        self.size = self.size -1
+        return
+
+    def deleteval(self,value):
+        index = self.findvalue(value)
+        self.deleteindex(index)
+        return
+
+    def pop(self,index = None):
+        if index > self.size - 1: raise IndexError("choose index lower than size of array")
+        if index !=None: self.array.pop(index)
+        elif index ==None: self.array.pop()
+        self.size -=1
+
+    def select(self,index):
+        return self.array[index]
+
+    def min(self):
+        return self.array[0]
+
+    def max(self):
+        return self.array[self.size-1]
+
+    def return_predecessor(self,index):
+        return self.array[index -1]
+
+    def return_successor(self,index):
+        return self.array[index +1]
+
+    def rank(self,value):
+        return self.findrank(value,0,self.size - 1)
+
+    def findrank(self,value,minindex,maxindex):
+        if minindex == maxindex:
+            return minindex
+        elif maxindex < minindex:
+            return minindex
+        for i in range(minindex,maxindex+1):
+            if value == self.array[maxindex+1//2]:
+                return (maxindex+1)//2
+            elif value > self.array[(maxindex+1)//2]:
+                return self.findvalue(value,(maxindex+1)//2 +1,maxindex)
+            else:
+                return self.findvalue(value,minindex,(maxindex+1)//2)
+
+    def find(self,value):
+        return self.findvalue(value,0,self.size-1)
+
+    def findvalue(self,value,minindex,maxindex):
+        if minindex == maxindex:
+            if value == self.array[minindex] :
+                return minindex
+            else:
+                return "value not found"
+        elif maxindex < minindex :
+            return "value not found"
+        for i in range(minindex,maxindex+1):
+            if value == self.array[maxindex+1//2]:
+                return (maxindex+1)//2
+            elif value > self.array[(maxindex+1)//2]:
+                return self.findvalue(value,(maxindex+1)//2 +1,maxindex)
+            else:
+                return self.findvalue(value,minindex,(maxindex+1)//2)
+
+if __name__ == "__main__":
+    a = SortedArray()
