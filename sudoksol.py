b9e3a7e322bfd106a8486828b188f4841bf1646b
Rahul Madhavan <rahul.maddy@gmail.com>
2016-08-26 17:12:15 +0530

    ok now we are fast!!

--- sudoksol.py
+++ sudoksol.py
@@ -1,6 +1,16 @@
 import time
-
+import sys
 numobjects = 0
+
+
+def twopowers(x):
+    powers = []
+    i = 1
+    while i <= x:
+        if i & x:
+            powers.append(i)
+        i <<= 1
+    return powers
 
 def is_power2(num):
     # states if a number is a power of two
@@ -11,6 +21,7 @@ def cross(A,B):
     return [a+b for a in A for b in B]
 
 digits   = '123456789'
+bindigits = 511
 rows     = 'ABCDEFGHI'
 cols     = digits
 squares  = cross(rows, cols)
@@ -19,15 +30,18 @@ unitlist = ([cross(rows, c) for c in cols] +
                 [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
 units = dict((s, [u for u in unitlist if s in u])             for s in squares)
 peers = dict((s, set(sum(units[s],[]))-set([s]))                 for s in squares)
-    
+
+#len of binary using table lookup O(1)
+onestable = [bin(i)[2:].count('1') for i in range(512)]
+
 
 def parse_grid(grid):
     """Convert grid to a dict of possible values, {square: digits}, or
     return False if a contradiction is detected."""
     ## To start, every square can be any digit; then assign values from the grid.
-    values = dict((s, digits) for s in squares)
+    values = dict((s, bindigits) for s in squares)
     for s,d in grid_values(grid).items():
-        if d in digits and not assign(values, s, d):
+        if d in digits and not assign(values, s, 2**int(d)):
             return False ## (Fail if we can't assign d to square s.)
     return values
 
@@ -40,34 +54,37 @@ def grid_values(grid):
 def assign(values, s, d):
     """Eliminate all the other values (except d) from values[s] and propagate.
     Return values, except return False if a contradiction is detected."""
-    other_values = values[s].replace(d, '')
-    if all(eliminate(values, s, d2) for d2 in other_values):
+    other_values = values[s]&(511-d)
+    if eliminate(values, s, other_values):
         return values
     else:
         return False
 
+
 def eliminate(values, s, d):
     """Eliminate d from values[s]; propagate when values or places <= 2.
     Return values, except return False if a contradiction is detected."""
-    if d not in values[s]:
+    if d & values[s] == 0:
         return values ## Already eliminated
-    values[s] = values[s].replace(d,'')
+    values[s] = values[s] & (511-d) #.replace(d,'')
     ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-    if len(values[s]) == 0:
+    if onestable[values[s]] <= 0:
         return False ## Contradiction: removed last value
-    elif len(values[s]) == 1:
+    elif onestable[values[s]] == 1:
         d2 = values[s]
         if not all(eliminate(values, s2, d2) for s2 in peers[s]):
             return False
     ## (2) If a unit u is reduced to only one place for a value d, then put it there.
-    for u in units[s]:
-        dplaces = [s for s in u if d in values[s]]
-        if len(dplaces) == 0:
-            return False ## Contradiction: no place for this value
-        elif len(dplaces) == 1:
-            # d can only be in one place in unit; assign it there
-            if not assign(values, dplaces[0], d):
-                return False
+
+    for tupower in twopowers(d):
+        for u in units[s]:
+            dplaces = [s for s in u if tupower & values[s] != 0]
+            if len(dplaces) == 0:
+                return False ## Contradiction: no place for this value
+            elif len(dplaces) == 1:
+                # d can only be in one place in unit; assign it there
+                if not assign(values, dplaces[0], tupower):
+                    return False
     return values
 
 def solve(grid):
@@ -78,19 +95,95 @@ def search(values):
     "Using depth-first search and propagation, try all possible values."
     if values is False:
         return False ## Failed earlier
-    if all(len(values[s]) == 1 for s in squares):
+    if all(is_power2(values[s]) == 1 for s in squares):
         return values ## Solved!
     ## Chose the unfilled square s with the fewest possibilities
-    n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    n,s = min((onestable[values[s]], s) for s in squares if not is_power2(values[s]))
     numobjects +=1
     return some(search(assign(values.copy(), s, d))
-		for d in values[s])
+		for d in twopowers(values[s]))
 
 def some(seq):
     "Return some element of seq that is true."
     for e in seq:
         if e: return e
     return False
+
+
+
+######################################################
+# VERY PRETTY DISPLAY FROM NORVIG
+######################################################
+
+
+def display(values):
+    "Display these values as a 2-D grid."
+    width = 1+max(len(values[s]) for s in squares)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print( ''.join(values[r+c].center(width)+('|' if c in '36' else ''))
+                      for c in cols)
+        if r in 'CF': print( line)
+    print()
+
+
+######################################################
+# SOME BINDERS
+######################################################
+def solve_all(grids, name='', showif=0.0):
+    """Attempt to solve a sequence of grids. Report results.
+    When showif is a number of seconds, display puzzles that take longer.
+    When showif is None, don't display any puzzles."""
+    def time_solve(grid):
+        start = time.clock()
+        values = solve(grid)
+        t = time.clock()-start
+        ## Display puzzles that take long enough
+        if showif is not None and t > showif:
+            display(grid_values(grid))
+            if values: display(values)
+            print('(%.2f seconds)\n' % t)
+        return (t, solved(values))
+    times, results = zip(*[time_solve(grid) for grid in grids])
+    N = len(grids)
+    if N > 1:
+        print( "Solved %d of %d %s puzzles (avg %.2f secs (%d Hz), max %.2f secs)." % (
+            sum(results), N, name, sum(times)/N, N/sum(times), max(times)))
+
+def solved(values):
+    "A puzzle is solved if each unit is a permutation of the digits 1 to 9."
+    def unitsolved(unit): return set(values[s] for s in unit) == set(digits)
+    return values is not False and all(unitsolved(unit) for unit in unitlist)
+
+def from_file(filename, sep='\n'):
+    "Parse a file into a list of strings, separated by sep."
+    return open(filename).read().strip().split(sep)
+
+
+######################################################
+# GENERATE PUZZLES
+######################################################
+import random
+def random_puzzle(N=17):
+    """Make a random puzzle with N or more assignments. Restart on contradictions.
+    Note the resulting puzzle is not guaranteed to be solvable, but empirically
+    about 99.8% of them are solvable. Some have multiple solutions."""
+    values = dict((s, digits) for s in squares)
+    for s in shuffled(squares):
+        if not assign(values, s, random.choice(values[s])):
+            break
+        ds = [values[s] for s in squares if len(values[s]) == 1]
+        if len(ds) >= N and len(set(ds)) >= 8:
+            return ''.join(values[s] if len(values[s])==1 else '.' for s in squares)
+    return random_puzzle(N) ## Give up and make a new puzzle
+
+def shuffled(seq):
+    "Return a randomly shuffled copy of the input sequence."
+    seq = list(seq)
+    random.shuffle(seq)
+    return seq
+
+
 
 if __name__ == "__main__":
     starttiem = time.time()
@@ -99,3 +192,6 @@ if __name__ == "__main__":
         solve(input_string)
     print("timetaken = ",time.time()-starttiem)
     print("num objects created = ",numobjects)
+    solve_all(from_file("data/toughsudokupuzzles.txt"), "hardest", None)
+    solve_all([random_puzzle() for _ in range(99)], "random", 100.0)
+    print("tot obj = ",numobjects)
